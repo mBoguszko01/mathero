c3051eaa6b51577709bac8f401c6557bcca57163
MICHAL BOGUSZKO <mic.boguszko@gmail.com>
2025-10-26 17:30:12 +0100

    signin page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,4 +1,54 @@
+import { useState } from "react";
+import { Link } from "react-router";
+import "../styles/Signin.css";
+
 const Signin = () => {
-  return <h1>Signin page</h1>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.email === "" || formData.password === "") {
+      setMessage("Proszę wypełnić wszystkie pola.");
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      setMessage("Nieprawidłowy format email.");
+    } else {
+      setMessage(" ");
+    }
+  };
+  return (
+    <div className="signin-wrapper">
+      <div className="signin-container">
+        <h1 className="signin-page-title">Zaloguj się</h1>
+        {message && <p>{message}</p>}
+        <form className="signin-form" onSubmit={handleSubmit}>
+          <div>
+            <label>Email</label>
+            <input type="text" name="email" onChange={handleChange} />
+          </div>
+          <div>
+            <label>Hasło</label>
+            <input type="text" name="password" onChange={handleChange} />
+          </div>
+          <Link className="sign-in-reset-password" onClick={()=>{alert("Funkcjonalność niezaimplementowana")}}>Zresetuj hasło</Link>
+          <button type="submit" className="signin-btn signin-main-btn">
+            Zaloguj się
+          </button>
+        </form>
+        <div style={{display:"flex", flexDirection:"column"}}>
+          <Link to="/">
+            <button className="signin-btn signin-second-btn">Wstecz</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Signin;
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import Signup from "../pages/Signup.jsx";
+import Signin from "../pages/Signin.jsx";
 
 export const AppRouter = () => {
     return (
@@ -8,7 +9,7 @@ export const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/signup" element={<Signup />}/>
-                <Route path="/signin" element={<div>Signin Page</div>}/>
+                <Route path="/signin" element={<Signin />}/>
             </Routes>
         </BrowserRouter>
     );
