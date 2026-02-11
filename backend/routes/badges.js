7ebb02ec3ad5a1b33305bee547936b37839b55e3
MICHAL BOGUSZKO <mic.boguszko@gmail.com>
2026-02-11 06:20:09 +0100

    getting badge data with current progress of badge

--- backend/routes/badges.js
+++ backend/routes/badges.js
@@ -19,25 +19,44 @@ export default function badgeRoutes(pool) {
                 WHERE user_id = $1`,
         [userId],
       );
+      const badgesProgressData = await pool.query(
+        `SELECT * FROM user_badge_progress WHERE user_id = $1`,
+        [userId],
+      );
+      //dopisz do allBadges progress_value, start_date, last_update, completed_at, days_to_complete,avg_daily_gain
+      const badgeTablesCombined = allBadges.rows.map((badge) => {
+        const b = badgesProgressData.rows.find((b) => b.badge_id === badge.id);
+        return {
+          ...badge,
+          progress_value: b?.progress_value || 0,
+          start_date: b?.start_date || null,
+          last_update: b?.last_update || null,
+          completed_at: b?.completed_at || null,
+          days_to_complete: b?.days_to_complete || null,
+          avg_daily_gain: b?.avg_daily_gain || null,
+        };
+      });
 
-      const markIfUnlocked = allBadges.rows.map((badge) => {
+      const markIfUnlocked = badgeTablesCombined.map((badge) => {
         if (usersBadges.rows.some((b) => b.badge_id === badge.id)) {
           return { ...badge, isUnlocked: true };
         }
         return { ...badge, isUnlocked: false };
       });
+
       const badgesMap = markIfUnlocked.reduce((acc, el) => {
         const key = el.badge_key;
         (acc[key] ||= []).push(el);
         return acc;
       }, {});
 
+
       return res.json({
         userId,
         badgesMap,
       });
     } catch (error) {
-      res.status(500).json({ message: "Błąd serwera" });
+      res.status(500).json({ message: `Błąd serwera: ${error.message}` });
     }
   });
   return router;
